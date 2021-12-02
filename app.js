04111c0016b226d1bbff00c8724c15f3da447183
Nikka Piel Francisco <nikkapiel.francisco.iics@ust.edu.ph>
2021-12-02 11:55:56 +0800

    updated app.js and log.html for url purposes
    updated app.js and log.html for url purposes

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.get('/owner/schedInfo', (req,res)=>{
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
-    console.log(Server running on port ${PORT});
+    console.log(`Server running on port ${PORT}`);
 });
 // connect to database
 connectToDB();
